c98f12bd71a0c2480fffd9a983b2f3be03f2e060
yaseyldrr <116019129+yaseyldrr@users.noreply.github.com>
2024-12-02 18:47:47 +0300

    updated

--- 02_Variables/Program.cs
+++ 02_Variables/Program.cs
@@ -10,6 +10,68 @@ namespace _02_Variables
     {
         static void Main(string[] args)
         {
+            #region Double
+
+            //double number; // declared
+            //number = 4.85; // Kodda nokta, çıktıda virgül olarak görürüz
+            //Console.WriteLine(number);
+
+            //Console.WriteLine("***** PRICE LIST *****");
+            //Console.WriteLine();
+
+            //double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberryPrice = 45;
+            //potatoPrice = 9.74;
+            //tomatoPrice = 6.88;
+            //// ctrl + space (otomatik tamamlar)
+
+            //Console.OutputEncoding = System.Text.Encoding.Unicode;
+            //// Unicode ayarlamak için kullanılır (özel karakterler için örn: ş, ğ, ₺)
+            //Console.WriteLine("--Apple Unit Price:  " + applePrice + " ₺"); // Altgr + T
+            //Console.WriteLine("--Orange Unit Price:  " + orangePrice + " ₺");
+            //Console.WriteLine("--Strawberry Unit Price:  " + strawberryPrice + " ₺");
+            //Console.WriteLine("--Patato Unit Price:  " + potatoPrice + " ₺");
+            //Console.WriteLine("--Tomato Unit Price:  " + tomatoPrice + " ₺");
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //double appleGram, orangeGram, strawGram, potatoGram, tomatoGram;
+
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawGram = 0.750;
+            //potatoGram = 4.859;
+            //tomatoGram = 3.745;
+
+            //double appleTotalPrice = applePrice * appleGram;
+            //double orangeTotalPrice = orangePrice * orangeGram;  
+            //double strawberryTotalPrice = strawberryPrice * strawGram;
+            //double potatoTotalPrice = potatoPrice * potatoGram;
+            //double tomatoTotalPrice = tomatoPrice * tomatoGram;
+
+            //Console.WriteLine( "Purchased Item: Apple - " + "Unit Price: " + applePrice + " - Gram: " + appleGram + " - Total: " + appleTotalPrice + " ₺");
+            //Console.WriteLine( "Purchased Item: Orange - " + "Unit Price: " + orangePrice + " - Gram: " + orangeGram + " - Total: " + orangeTotalPrice + " ₺");
+            //Console.WriteLine( "Purchased Item: Strawberry - " + "Unit Price: " + strawberryPrice + " - Gram: " + strawGram + " - Total: " + strawberryTotalPrice + " ₺");
+            //Console.WriteLine( "Purchased Item: Potato - " + "Unit Price: " + potatoPrice + " - Gram: " + potatoGram + " - Total: " + potatoTotalPrice + " ₺");
+            //Console.WriteLine( "Purchased Item: Tomato - " + "Unit Price: " + tomatoPrice + " - Gram: " + tomatoGram + " - Total: " + tomatoTotalPrice + " ₺");
+
+            //double totalPurchase = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice + tomatoTotalPrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine("Total Cost: " + totalPurchase + " ₺");
+
+            #endregion
+            #region Char
+
+
+
+
+
+            #endregion
+            Console.Read();
         }
     }
 }
