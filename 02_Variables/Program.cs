9e156478aa81d7192abc40b41aee4d894bc44ef6
yaseyldrr <116019129+yaseyldrr@users.noreply.github.com>
2024-12-04 16:17:33 +0300

    updated

--- 02_Variables/Program.cs
+++ 02_Variables/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -64,13 +65,111 @@ namespace _02_Variables
             //Console.WriteLine("Total Cost: " + totalPurchase + " ₺");
 
             #endregion
+
             #region Char
 
+            //char symbol = 'A';
+            //Console.WriteLine(symbol);
 
+            #endregion
 
+            #region Keyboard Input(String)
+
+            //Console.WriteLine("******C# Airlines passenger Information*****");
+            //Console.WriteLine();
+
+            //// ctrl + f ile toplu isim değiştirebilirsin
+            //string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge, passengerID;
+
+            //Console.Write("Passenger Name: ");
+            //passengerName = Console.ReadLine();
+
+            //Console.Write("Passenger Surname: ");
+            //passengerSurname = Console.ReadLine();
+
+            //Console.Write("Passenger District: ");
+            //passengerDistrict = Console.ReadLine();
+
+            //Console.Write("Passenger City: ");
+            //passengerCity = Console.ReadLine();
+
+            //Console.Write("Passenger Age: ");
+            //passengerAge = Console.ReadLine();
+
+            //Console.Write("Passenger ID: ");
+            //passengerID = Console.ReadLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine("----------------------");
+            //Console.WriteLine("ID: " + passengerID);
+            //Console.WriteLine("Passenger: " + passengerName + " " + passengerSurname);
+            //Console.WriteLine("Location: " + passengerDistrict + "/" + passengerCity);
+            //Console.WriteLine("Age: " + passengerAge);
+
+            #endregion
+
+            #region Keyboard Input(Integer) and Conversion
+
+            // C# programlama dili console dan alınan değerlerin hepsini string olarak alır;
+            // yani bir alfabetik değer olarak alır, bunu dönüştürmek gerekir.
+
+            //int shoesPrice, computerPrice, chairPrice, tvPrice;
+            //shoesPrice = 1000;
+            //computerPrice = 20000;
+            //chairPrice = 5000;
+            //tvPrice = 12000;
+
+            //int shoesCount, computerCount, chairCount, tvCount;
+
+            //Console.Write("Please enter the total number of shoes you have purchased: ");
+            //shoesCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Please enter the total number of computer you have purchased: ");
+            //computerCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Please enter the total number of chair you have purchased: ");
+            //chairCount  = int.Parse(Console.ReadLine());
+
+            //Console.Write("Please enter the total number of TV you have purchased: ");
+            //tvCount = int.Parse(Console.ReadLine());
+
+            //int totalPrice = (shoesCount*shoesPrice) + (computerCount*computerPrice)
+            //    + (chairCount*chairPrice) + tvCount*tvPrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine("Total Price: " + totalPrice);
 
 
             #endregion
+
+            #region Keyboard Input(Decimal Numbers) and Operations
+
+            //double exam1, exam2, exam3, averageGrade;
+
+            //Console.WriteLine("Please enter 1st exam grade: ");
+            //exam1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter 2nd exam grade: ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please enter 3rd exam grade: ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+
+            //averageGrade = (exam1 + exam2 + exam3)/3;
+            //Console.WriteLine("Your average grade for the exams is: " + averageGrade);
+
+            #endregion
+
+            #region Keyboard Input(Character)
+
+            //char gender;
+            //Console.WriteLine("Select a gender");
+            //gender = char.Parse(Console.ReadLine());
+            //Console.WriteLine("Gender: " + gender);
+
+            #endregion
+
             Console.Read();
         }
     }
